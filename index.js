a2a27b52105aa0fe09f4ee9b13a2a27ffd477a62
AzanSaleem69 <164613997+AzanSaleem69@users.noreply.github.com>
2024-05-16 17:23:44 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+
+
+function add(){
+    const num1= parseInt(prompt("Enter num 1"));
+    const num2= parseInt(prompt("Enter num 2"));
+    const total=num1+num2;
+    alert(total);
+}
+
+
+function mul(){
+    const num1= parseInt (prompt("Enter num 1"));
+    const num2=  parseInt (prompt("Enter num 2"));
+    const total=num1*num2;
+    alert(total);
+}
+
+
+function div(){
+    const num1= parseInt (prompt("Enter num 1"));
+    const num2=  parseInt (prompt("Enter num 2"));
+    const total=num1/num2;
+    alert(total);
+}
+
+function sub(){
+    const num1= parseInt (prompt("Enter num 1"));
+    const num2=  parseInt (prompt("Enter num 2"));
+    const total=num1-num2;
+    alert(total);
+}
+// // for(let i=0;i<4;i++){
+// // const num= prompt("Enter the numbers")
+// // if(num%2==0)
+// //     {
+// //         console.log(`Even number are${num}`)
+// //     }
+// //     else {
+// //         console.log(`odd numbers${num}`)
+// //     }
+// // }
+
+
+// const s=0;
+// function even(s)
+// {
+//     const a=prompt("Enter number of inputs")
+//     for(let i=0;i<a;i++){
+//         const num= prompt("Enter the numbers")
+//         if(num%2==0)
+//             {
+//                 console.log(`Even number are${num}`)
+//             }
+//             else {
+//                 console.log(`odd numbers${num}`)
+//             }
+//         }
+// }
+// even(s);
+
+// const l=0;
+// function positive(l)
+// {
+//     const a=prompt("Enter number of inputs")
+//     for(let i=0;i<l;i++){
+//         const num= prompt("Enter the numbers")
+//         if(num>0)
+//             {
+//                 console.log(`positive numbers are${num}`)
+//             }
+//             else {
+//                 console.log(`negative numbers${num}`)
+//             }
+//         }
+// }
+// positive(l);
+
+
+// const u=0;
+// function total(u)
+// {
+//     const a=prompt("Enter number of inputs")
+
+//       console.log(`Total numbers${a}`)   
+// }
+// total(u);
